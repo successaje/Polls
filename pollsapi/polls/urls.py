39bcfa6e6280069dfe20efb81672b90eaa834ce1
Success Aje <successaje7@gmail.com>
2022-02-02 23:52:02 +0100

    updates

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -3,6 +3,8 @@ from .views import polls_list, polls_detail
 
 from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 
+#There are 3 API endproints
+
 urlpatterns = [
     path('polls/', PollList.as_view(), name='polls_list'),
     path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
--- pollsapi/polls/views.py
+++ pollsapi/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, \
+    get_object_or_404
 from django.http import JsonResponse
 
 from .models import Poll
